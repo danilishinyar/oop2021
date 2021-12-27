013c6a79c46889e533daa5cc08e50bdb71c0f5bf
Yaroslav Danilishin <danilishin.yar@gmail.com>
2021-12-27 17:24:20 +0300

    fixed structure

--- 2/tests/cmake-build-debug/lib/googletest/googlemock/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /Users/danilishinyar/OOP/lab2_oop/tests/lib/googletest/googlemock
-# Build directory: /Users/danilishinyar/OOP/lab2_oop/tests/cmake-build-debug/lib/googletest/googlemock
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("../googletest")
--- 2/tests/cmake-build-debug/lib/googletest/googletest/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /Users/danilishinyar/OOP/lab2_oop/tests/lib/googletest/googletest
-# Build directory: /Users/danilishinyar/OOP/lab2_oop/tests/cmake-build-debug/lib/googletest/googletest
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
